6d192a859d327380cf3d7808fa430b472c7576b2
elvinaliyev6 <aliyevaliyevelvin@gmail.com>
2022-04-05 13:22:50 +0400

    changes

--- ResumeAppByUsingSpring/ResumeDbAppJPASpring/src/test/java/com/company/ResumeDbAppJpaSpringApplicationTests.java
+++ ResumeAppByUsingSpring/ResumeDbAppJPASpring/src/test/java/com/company/ResumeDbAppJpaSpringApplicationTests.java
@@ -1,7 +1,9 @@
 package com.company;
 
-import org.junit.Test;
+//import org.junit.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testng.annotations.Test;
 
 @SpringBootTest
 class ResumeDbAppJpaSpringApplicationTests {
